fcba6160f3af78c451aa634777026ed2b2691ce0
elad-rahamim <elad@eready.co.il>
2015-06-20 21:14:38 +0300

    Create repository for Table View App
    Create repository for Table View App

---
+++ Create repository for Table View App/Create repository for Table View AppTests/Create_repository_for_Table_View_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Create_repository_for_Table_View_AppTests.swift
+//  Create repository for Table View AppTests
+//
+//  Created by Elad Rahmim on 6/20/15.
+//  Copyright (c) 2015 Elad Rahmim. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Create_repository_for_Table_View_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
